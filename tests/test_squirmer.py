695ace083dbbb1cc1ae771255038ec1fc97450a1
Robin <robin.roth@etu.unistra.fr>
2024-04-19 11:19:56 +0200

    Added workflows, first small test, requirements

---
+++ tests/test_squirmer.py
@@ -0,0 +1,13 @@
+from Code.interactingsquirmers import InteractingSquirmers
+from Code.squirmer import Squirmer
+import numpy as np
+
+def test_sq():
+    x, y = 0.5, 0.2
+    orient = np.pi
+    a = 0.05
+    beta = -2
+    v0 = 0.7
+    squirmer = Squirmer(x,y,orient,a,beta,v0)
+    assert squirmer.B1 == (2/3)*v0
+    assert squirmer.B2 == beta*(2/3)*v0
