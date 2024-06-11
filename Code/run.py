633a47f5c06474eafa137d1ba79c1f548243111e
Robin <robin.roth@etu.unistra.fr>
2024-06-11 12:16:28 +0200

    simulations and tests

--- Code/run.py
+++ Code/run.py
@@ -1,5 +1,8 @@
 import argparse
 import numpy as np
+from squirmer import Squirmer
+from interactingsquirmers import InteractingSquirmers
+from plot import plot_squirmers_positions
 from simulation import sim_intercating_squirmers, sim_vid_interact_squir, sim_squirmer_border
 
 def main(simulation, filename):
@@ -40,14 +43,19 @@ def main(simulation, filename):
     #amplitude of orientational interactions
     Eo = ((3./10.)*v0/a)
     #distance of steric interactions
-    ds = 2**(7./6)*a
+    ds = a
     #viscovity parameter
     mu = 0.01
+    squirmer1 = Squirmer(x1, y1, orient1, a, beta0, v0)
+    squirmer2 = Squirmer(x2, y2, orient2[6], a, beta0, v0)
+    intera = InteractingSquirmers(squirmer1, squirmer2, R, dt, dt_out, T, Es, ds, mu, Eo, lnEps_cr)
+    history = intera.loop_time()
 
+    plot_squirmers_positions(R, history, 'check')
     if simulation == 'interact_sq':
         sim_intercating_squirmers(x1, y1, x2, y2, orient1, orient2, a, beta0, betainf, betasup, v0, R, dt, dt_out, T, Es, ds, mu, Eo, lnEps_cr, file, dir='graphs')
     elif simulation == 'vid_interact_sq':
-        sim_vid_interact_squir(x1, y1, x2, y2, orient1, orients[1], a, betas[0], v0, R, dt, dt_out, T, Es, ds, mu, Eo, lnEps_cr, filename, dir='videos', gif=False)
+        sim_vid_interact_squir(x1, y1, x2, y2, orient1, orient2[6], a, betas[0], v0, R, dt, dt_out, T, Es, ds, mu, Eo, lnEps_cr, filename, dir='videos', gif=False)
     elif simulation == 'sq_border':
         sim_squirmer_border(xs, ys, orients, a, betas, v0, R, dt, dt_out, T, Es, ds, mu, Eo, lnEps_cr, filename, dir='graphs')
 
