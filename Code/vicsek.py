234faf53ad38092f9663fd5d413d1686a4c915e6
Robin <robin.roth@etu.unistra.fr>
2024-06-14 11:04:18 +0200

    test update

--- Code/vicsek.py
+++ Code/vicsek.py
@@ -3,7 +3,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-from squirmer import Squirmer
+from Code.squirmer import Squirmer
 
 class Vicsek_continous:
 
