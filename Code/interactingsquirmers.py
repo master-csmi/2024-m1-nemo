875925f7eccbf550639cde9abd8bc067f27d7c9f
Robin <robin.roth@etu.unistra.fr>
2024-06-18 11:30:48 +0200

    verified intercating squirmers

--- Code/interactingsquirmers.py
+++ Code/interactingsquirmers.py
@@ -274,51 +274,50 @@ class InteractingSquirmers:
         plot_squirmers_positions(self.R, history, filename_pos)
         plot_dist_sq(history, filename_dist)
 
-#velocity
-v0 = 1
-#length of the square
-L = 2
-#half of the length of the square
-R = L/2
-#squirmers' radius
-a = 0.15
-#coordinates
-x1, y1 = -2*a, 0
-x2, y2 = a/1.1, 0
-#orientations
-orient1 = np.pi/2
-orient2 = [-np.pi/4, np.pi/4, np.pi/2, -np.pi/2, 3*np.pi/4, -3*np.pi/4, np.pi, 2*np.pi]
-#border simulation
-xs = [-0.6, 0.6, 0.2, -0.2]
-ys = [-0.2, -0.2, 0.6, -0.6]
-orients = [3*np.pi/4, np.pi/4, 3*np.pi/4, -np.pi/4]
-#files labels
-file = ["-pi.4", "pi.4", "pi.2", "-pi.2", "3pi.4", "-3pi.4", "pi", "2pi"]
-#betas
-betas = [0, -7.5, 7.5]
-beta0, betainf, betasup = 0, -7.5, 7.5
-#time-step
-dt = 1e-4
-#cut-off for -log
-lnEps_cr = np.exp(-5)
-#amplitude of steric interactions
-Es = 1
-#simulation time
-T = 1
-#periodicity of outputs
-dt_out = 0.05
-#amplitude of orientational interactions
-Eo = ((3./10.)*v0/a)
-#distance of steric interactions
-ds = 2**(7./6)*a
-#viscovity parameter
-mu = 0.01
+# #velocity
+# v0 = 1
+# #length of the square
+# L = 2
+# #half of the length of the square
+# R = L/2
+# #squirmers' radius
+# a = 0.15
+# #coordinates
+# x1, y1 = -2*a, 0
+# x2, y2 = a/1.1, 0
+# #orientations
+# orient1 = np.pi/2
+# orient2 = [-np.pi/4, np.pi/4, np.pi/2, -np.pi/2, 3*np.pi/4, -3*np.pi/4, np.pi, 2*np.pi]
+# #border simulation
+# xs = [-0.6, 0.6, 0.2, -0.2]
+# ys = [-0.2, -0.2, 0.6, -0.6]
+# orients = [3*np.pi/4, np.pi/4, 3*np.pi/4, -np.pi/4]
+# #files labels
+# file = ["-pi.4", "pi.4", "pi.2", "-pi.2", "3pi.4", "-3pi.4", "pi", "2pi"]
+# #betas
+# betas = [0, -7.5, 7.5]
+# beta0, betainf, betasup = 0, -7.5, 7.5
+# #time-step
+# dt = 1e-4
+# #cut-off for -log
+# lnEps_cr = np.exp(-5)
+# #amplitude of steric interactions
+# Es = 1
+# #simulation time
+# T = 1
+# #periodicity of outputs
+# dt_out = 0.05
+# #amplitude of orientational interactions
+# Eo = ((3./10.)*v0/a)
+# #distance of steric interactions
+# ds = 2**(7./6)*a
+# #viscovity parameter
+# mu = 0.01
 
-N = 2
-xs2 = [x1, x2]
-ys2 = [y1, y2]
-orients2 = [orient1, orient2[2]]
+# N = 2
+# xs2 = [x1, x2]
+# ys2 = [y1, y2]
+# orients2 = [orient1, orient2[2]]
 
-inter = InteractingSquirmers(N, xs2, ys2, orients2, a, beta0, v0, R, dt, dt_out, T, Es, ds, mu, Eo, lnEps_cr)
-inter.loop_time()
-print(inter.squirmers[0].y)
+# inter = InteractingSquirmers(N, xs2, ys2, orients2, a, beta0, v0, R, dt, dt_out, T, Es, ds, mu, Eo, lnEps_cr)
+# inter.loop_time()
