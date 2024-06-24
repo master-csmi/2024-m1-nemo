5bb6384f66ba57e6a1f71dfd485abd926377a32d
Robin <robin.roth@etu.unistra.fr>
2024-06-24 09:47:04 +0200

    checks

--- Code/run.py
+++ Code/run.py
@@ -58,7 +58,7 @@ def main(simulation, N, filename):
     if simulation == 'video':
         sim_vid_interact_sq(N, xs, ys, orients, a, beta, v0, R, dt, dt_out, T, Es, ds, mu, Eo[0][0], lnEps_cr, border, filename, dir='videos')
     elif simulation == 'plot':
-        sim_interacting_squirmers(N, xs, ys, orients, a, beta, v0, R, dt, dt_out, T, Es, ds, mu, Eo[0][0], lnEps_cr, border, False, filename, dir='graphs')
+        sim_interacting_squirmers(N, xs, ys, orients, a, beta, v0, R, dt, dt_out, T, Es, ds, mu, Eo[0][0], lnEps_cr, border, False, filename, border_plot, dir='graphs')
     elif simulation == 'border':
         xs = [-0.4]
         ys = [-0.7]
